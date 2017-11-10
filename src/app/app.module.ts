1c2353420eebe74e8969dcada5cf22c32e4f8b48
FranciscoMH <fmelendezherize@gmail.com>
2017-11-10 12:06:49 -0400

    fix http client in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -15,7 +15,7 @@ import { AuthService } from './services/auth.service';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent }
     ])
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
-import { URLSearchParams } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+//import { URLSearchParams } from '@angular/http';
+
+//import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class AuthService {
@@ -9,7 +11,7 @@ export class AuthService {
   private BASE_URL: string = 'http://127.0.0.1:5555/api/v1/auth';
   private headers: Headers = new Headers({'Content-Type': 'application/json'});
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   test(): string {
     return 'working routing';
@@ -21,21 +23,40 @@ export class AuthService {
 
     let url: string = 'http://127.0.0.1:5555/api/v1/auth/accounts/';
 
-    let urlSearchParams = new URLSearchParams();
-    urlSearchParams.append('email', user.email);
-    urlSearchParams.append('password', user.password);
-
-    //return this.http.post(url, urlSearchParams).toPromise();
-
-    this.http.get(url).subscribe(
-      data => {
-        alert('ok');
+    this.http
+    .get(url)
+    .subscribe(
+      data => { 
+        alert('ok'); 
+        console.log(data);
       },
-      error => {
-        console.log(error);
-        console.log(JSON.stringify(error.json()));
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          // A client-side or network error occurred. Handle it accordingly.
+          console.log('An error occurred:', err.error.message);
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+        }
       }
-    )
+    );
+
+    // let urlSearchParams = new URLSearchParams();
+    // urlSearchParams.append('email', user.email);
+    // urlSearchParams.append('password', user.password);
+  
+    // //return this.http.post(url, urlSearchParams).toPromise();
+
+    // this.http.get(url).subscribe(
+    //   data => {
+    //     alert('ok');
+    //   },
+    //   error => {
+    //     console.log(error);
+    //     console.log(JSON.stringify(error.json()));
+    //   }
+    // )
 
     // this.http.post(url, urlSearchParams).subscribe(
     //   data => {
@@ -47,8 +68,8 @@ export class AuthService {
     // )
   }
 
-  register(user): Promise<any> {
-    let url: string = `${this.BASE_URL}/register`;
-    return this.http.post(url, user, {headers: this.headers}).toPromise();
-  }
+  // register(user): Promise<any> {
+  //   let url: string = `${this.BASE_URL}/register`;
+  //   return this.http.post(url, user, {headers: this.headers}).toPromise();
+  // }
 }
